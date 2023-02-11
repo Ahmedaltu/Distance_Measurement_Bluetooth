5e34b424dfa55881e14010670b68a745c7d8c9eb
Ahmed Al-Tuwaijari <76178825+Ahmedaltu@users.noreply.github.com>
2023-02-11 19:12:52 +0200

    Add files via upload

---
+++ nrf_dm/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/Users/ahmed/Documents/Wizense/nrf_dm;C:/ncs/v2.2.0-rc1/nrf;C:/ncs/v2.2.0-rc1/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/Users/ahmed/Documents/Wizense/nrf_dm/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "c:\ncs\toolchains\v2.2.0-rc1\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
